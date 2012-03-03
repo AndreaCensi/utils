e665b143985f3cd108828d170566acad2338f0bd
Andrea Censi <andrea@cds.caltech.edu>
2012-03-03 12:36:37 -0800

    utils

---
+++ src/acutils/misc/lenient_option_parser.py
@@ -0,0 +1,25 @@
+__version__ = '1.0'
+
+import optparse
+from optparse import IndentedHelpFormatter
+class LenientOptionParser(optparse.OptionParser):
+    
+    def parse_args(self, args):
+        self.arguments = list(args)
+        return optparse.OptionParser.parse_args(self, args)
+    
+    def error(self, msg):
+        #msg = '%s: %s' % (self.get_prog_name(), msg)
+        msg += ('\nArguments: %s %s' % 
+                (self.get_prog_name(), " ".join(self.arguments)))
+        raise UserError(msg)
+
+def OptionParser(prog, usage):
+    formatter = IndentedHelpFormatter(
+                 indent_increment=2,
+                 max_help_position=80,
+                 width=100,
+                 short_first=1)
+                               
+    return LenientOptionParser(formatter=formatter)
+
